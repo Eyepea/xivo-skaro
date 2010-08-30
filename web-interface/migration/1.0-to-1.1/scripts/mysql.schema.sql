8f922d35c8baaf09a9786f1b39b4a2d1994d16fb
Guillaume Bour <gbour@proformatique.com>
2010-08-30 15:39:02 +0000

    [evol] trunk upport / ticket #1222 : add table provisioning with registrar_(main|backup) and proxy_(main|backup) ips
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@9061 dc85baef-7018-0410-bfbd-8a639da648bb

--- web-interface/migration/1.0-to-1.1/scripts/mysql.schema.sql
+++ web-interface/migration/1.0-to-1.1/scripts/mysql.schema.sql
@@ -201,4 +201,16 @@ CREATE TABLE `ha_peer` (
  PRIMARY KEY (`iface`, `host`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+-- provisioning
+CREATE TABLE `provisioning` (
+ `id` tinyint(1) auto_increment,
+ `registrar_main`   varchar(255) NOT NULL DEFAULT '',
+ `registrar_backup` varchar(255) NOT NULL DEFAULT '',
+ `proxy_main`       varchar(255) NOT NULL DEFAULT '',
+ `proxy_backup`     varchar(255) NOT NULL DEFAULT '',
+ PRIMARY KEY(`id`)
+) ENGINE=MyISAM;
+
+INSERT INTO `provisioning` VALUES(1, '', '', '', '');
+
 COMMIT;
