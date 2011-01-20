a19860741d28733aacec4a84793fc420897d0fe5
Guillaume Bour <gbour@proformatique.com>
2011-01-20 16:15:52 +0000

    [fix] no need to prepend "macro-" prefix anymore
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@9988 dc85baef-7018-0410-bfbd-8a639da648bb

--- confgen/xivo_confgen/frontends/asterisk.py
+++ confgen/xivo_confgen/frontends/asterisk.py
@@ -497,7 +497,7 @@ class AsteriskFrontend(Frontend):
 			appdata = list(exten['appdata'].replace('|',',').split(','))
 			if app == 'Macro':
 				app     = 'Gosub'
-				appdata = ('macro-' + appdata[0], 's', '1(' + ','.join(appdata[1:]) + ')')
+				appdata = (appdata[0], 's', '1(' + ','.join(appdata[1:]) + ')')
 
 			print >>o, "exten = %s,%s,%s(%s)" % (exten['exten'], exten['priority'], app, ','.join(appdata))
 
