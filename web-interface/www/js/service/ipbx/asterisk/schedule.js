fbd886c0c57bc763e078e4318902c7d684bfcdf0
Guillaume Bour <gbour@proformatique.com>
2011-02-11 14:12:33 +0000

    schedules: manage dynamic dialactions in schedule form
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@10150 dc85baef-7018-0410-bfbd-8a639da648bb

--- web-interface/www/js/service/ipbx/asterisk/schedule.js
+++ web-interface/www/js/service/ipbx/asterisk/schedule.js
@@ -16,10 +16,57 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+var dialevent = 0;
+
+function xivo_ast_schedule_add_dyn_dialaction(dialname)	
+{
+	dialevent += 1;
+
+	xivo_elt_dialaction[dialname] = {};
+	xivo_fm_dialaction[dialname]  = {};
+
+	xivo_ast_build_dialaction_array(dialname);
+	if((action = dwho_eid('it-dialaction-'+dialname+'-actiontype')) !== false)
+		xivo_ast_chg_dialaction(dialname,action);
+}
+
+function xivo_ast_schedule_add_closed_action(name, obj)
+{
+
+	dialevent  += 1;
+
+//  'onclick="dwho.dom.make_table_list(\'disp2\',this); return(dwho.dom.free_focus());"',
+	new dwho.http('http://192.168.1.10/service/ipbx/ui.php/call_management/schedule?act=dialaction&event=' + dialevent,
+		{'callbackcomplete':	function(xhr) 
+			{ 
+				elt = dwho_eid('onclosed-time-dialaction');
+				elt.id = 'onclosed-time-dialaction-' + dialevent
+				elt.innerHTML = xhr.responseText;
+
+				dwho.dom.make_table_list(name, obj);
+
+				xivo_elt_dialaction[dialevent] = {};
+				xivo_fm_dialaction[dialevent]  = {};
+
+				xivo_ast_build_dialaction_array(dialevent);
+				if((action = dwho_eid('it-dialaction-'+dialevent+'-actiontype')) !== false)
+					xivo_ast_chg_dialaction(dialevent,action);
+
+				// restore model attributes after clone
+				elt.id = 'onclosed-time-dialaction';
+				elt.innerHTML = '';
+			},
+      'method':		'post',
+      'cache':			false
+		},
+    {},
+		true);
+
+}
+
 function xivo_ast_schedule_onload()
 {
-	xivo_ast_build_dialaction_array('inschedule');
-	xivo_ast_build_dialaction_array('outschedule');
+	xivo_ast_build_dialaction_array('schedule_fallback');
 	xivo_ast_dialaction_onload();
 }
 
