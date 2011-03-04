e52a1702d66dc8572c672da0598cc8a8d0d9f78a
Cédric Abunar <cabunar@proformatique.com>
2011-03-04 17:21:40 +0000

    [features] add a key in dwho_report object push method Group errors in a block for display and can now export a block format txt to an external analysis.
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@10317 dc85baef-7018-0410-bfbd-8a639da648bb

---
+++ web-interface/www/exportreport.php
@@ -0,0 +1,44 @@
+<?php
+
+#
+# XiVO Web-Interface
+# Copyright (C) 2006-2011  Proformatique <technique@proformatique.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 
+
+require_once('xivo.php');
+
+$bloc = isset($_QR['bloc']) === false ? '' : $_QR['bloc'];
+$type = isset($_QR['type']) === false ? '' : $_QR['type'];
+
+if(($result = dwho_report::get_bloc($type,$bloc)) === false)
+	die;
+
+header('Pragma: no-cache');
+header('Cache-Control: private, must-revalidate');
+header('Last-Modified: '.date('D, d M Y H:i:s',mktime()).' '.dwho_i18n::strftime_l('%Z',null));
+header('Content-Disposition: attachment; filename=xivo_report_'.$type.'-'.$bloc.'.txt');
+header('Content-Type: text/txt; charset=UTF-8');
+
+ob_start();
+
+while($result)
+	echo array_shift($result),"\n";
+
+header('Content-Length: '.ob_get_length());
+ob_end_flush();
+die();
+
+?>
