863d5f631fa4045f6dafce1fd5c955da4d5209cc
Guillaume Bour <gbour@proformatique.com>
2011-04-07 09:25:26 +0000

    [fix] update to use jquery-ui-lightness theme
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@10522 dc85baef-7018-0410-bfbd-8a639da648bb

--- web-interface/action/www/service/ipbx/asterisk/call_management/schedule.php
+++ web-interface/action/www/service/ipbx/asterisk/call_management/schedule.php
@@ -91,7 +91,7 @@ case 'add':
 		$_TPL->set_var('destination_list',$appschedule->get_dialaction_destination_list());
 
 		$dhtml = &$_TPL->get_module('dhtml');
-		$dhtml->set_css('extra-libs/jquery-ui/css/smoothness/jquery-ui.css', true);
+		$dhtml->set_css('extra-libs/jquery-ui/css/lightness/jquery-ui.css', true);
 		$dhtml->set_css('extra-libs/xivo.schedule/xivo.schedule.css'       , true);
 
 		$dhtml->set_js('extra-libs/mousewheel/jquery.mousewheel.js'  , true);
@@ -174,19 +174,12 @@ case 'add':
 		$_TPL->set_var('context_list',$appschedule->get_context_list());
 
 		$dhtml = &$_TPL->get_module('dhtml');
-		$dhtml->set_css('css/jquery/current/jquery-ui.css');
-		$dhtml->set_css('css/jquery/anytime.css');
-		$dhtml->set_css('css/jquery/jquery.xivo.schedule.css');
+		$dhtml->set_css('extra-libs/jquery-ui/css/lightness/jquery-ui.css', true);
+		$dhtml->set_css('extra-libs/xivo.schedule/xivo.schedule.css'       , true);
 
-		$dhtml->set_js('js/jquery.js');
-		$dhtml->set_js('js/jquery-ui.js');
-		$dhtml->set_js('js/jquery/ui/jquery.ui.core.js');
-		$dhtml->set_js('js/jquery/ui/jquery.ui.widget.js');
-		$dhtml->set_js('js/jquery/ui/jquery.ui.mouse.js');
-		$dhtml->set_js('js/jquery/ui/jquery.ui.slider.js');
-		$dhtml->set_js('js/jquery/jquery.mousewheel.js');
-		$dhtml->set_js('js/jquery/jquery.xivo.schedule.js');
-		$dhtml->set_js('js/jquery/jquery.xivo.schedule-fr.js');
+		$dhtml->set_js('extra-libs/mousewheel/jquery.mousewheel.js'  , true);
+		$dhtml->set_js('extra-libs/xivo.schedule/xivo.schedule.js'   , true);
+		$dhtml->set_js('extra-libs/xivo.schedule/xivo.schedule-fr.js', true);
 
 		$dhtml->set_js('js/dwho/uri.js');
 		$dhtml->set_js('js/dwho/http.js');
--- web-interface/tpl/www/bloc/meta.php
+++ web-interface/tpl/www/bloc/meta.php
@@ -41,8 +41,8 @@
 
 <script type="text/javascript" charset="utf-8" src="<?=$this->file_time($this->url('extra-libs/jquery/jquery.min.js'));?>"></script>
 <script type="text/javascript" charset="utf-8" src="<?=$this->file_time($this->url('extra-libs/jquery-ui/jquery-ui.min.js'));?>"></script>
-<script type="text/javascript" charset="utf-8" src="<?=$this->file_time($this->url('extra-libs/autocomplete/jquery.autocomplete.js'));?>"></script>
-<link rel="stylesheet" type="text/css" charset="utf-8" href="<?=$this->file_time($this->url('extra-libs/autocomplete/jquery.autocomplete.css'));?>">
+<script type="text/javascript" charset="utf-8" src="<?=$this->file_time($this->url('extra-libs/jquery-ui/ui/jquery.ui.autocomplete.js'));?>"></script>
+<link rel="stylesheet" type="text/css" charset="utf-8" href="<?=$this->file_time($this->url('extra-libs/jquery-ui/themes/ui-lightness/jquery.ui.autocomplete.css'));?>">
 <link rel="stylesheet" type="text/css" charset="utf-8" href="<?=$this->file_time($this->url('extra-libs/jquery-ui/css/ui-lightness/jquery-ui.css'));?>">
 
 <?php
