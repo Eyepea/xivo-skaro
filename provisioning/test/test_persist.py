01b9301a05a900c836a1e7aa26f449be184fb880
Etienne Desrochers-Lessard <hexanol@proformatique.com>
2011-05-02 19:20:28 +0000

    update some unit tests (some are still out-of-date, mostly those which make use of Deferred...)
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@10692 dc85baef-7018-0410-bfbd-8a639da648bb

--- provisioning/test/test_persist.py
+++ provisioning/test/test_persist.py
@@ -22,7 +22,7 @@ import unittest
 import provd.persist.memory as memory
 from provd.persist.common import ID_KEY
 from provd.persist.id import numeric_id_generator, urandom_id_generator
-from provd.persist.util import _select_value, _create_predicate_from_selector,\
+from provd.persist.util import _retrieve_doc_values, _create_pred_from_selector,\
     _create_value_matcher
 
 # TODO fix the test so they all work with the new async interface
@@ -104,37 +104,37 @@ class TestDictCollection(unittest.TestCase):
 class TestSelectorSelectValue(unittest.TestCase):
     def test_select_value_simple(self):
         doc = {'k': 'v'}
-        self.assertEqual(['v'], list(_select_value('k', doc)))
+        self.assertEqual(['v'], list(_retrieve_doc_values('k', doc)))
     
     def test_select_value_simple_with_noise(self):
         doc = {'k': 'v', 'foo': [{'bar': '555'}]}
-        self.assertEqual(['v'], list(_select_value('k', doc)))
+        self.assertEqual(['v'], list(_retrieve_doc_values('k', doc)))
     
     def test_select_value_simple_no_match(self):
         doc = {}
-        self.assertEqual([], list(_select_value('k', doc)))
+        self.assertEqual([], list(_retrieve_doc_values('k', doc)))
     
     def test_select_value_dict(self):
         doc = {'k': {'kk': 'v'}}
-        self.assertEqual(['v'], list(_select_value('k.kk', doc)))
+        self.assertEqual(['v'], list(_retrieve_doc_values('k.kk', doc)))
     
     def test_select_value_dict_3depth(self):
         doc = {'k': {'kk': {'kkk': 'v'}}}
-        self.assertEqual(['v'], list(_select_value('k.kk.kkk', doc)))
+        self.assertEqual(['v'], list(_retrieve_doc_values('k.kk.kkk', doc)))
     
     def test_select_value_list(self):
         doc = {'k': ['v1', 'v2']}
         self.assertEqual(['v1', 'v2'],
-                         list(_select_value('k', doc)))
+                         list(_retrieve_doc_values('k', doc)))
     
     def test_select_value_dict_inside_list(self):
         doc = {'k': [{'kk': 'v'}]}
-        self.assertEqual(['v'], list(_select_value('k.kk', doc)))
+        self.assertEqual(['v'], list(_retrieve_doc_values('k.kk', doc)))
     
     def test_select_value_dict_inside_list_multiple_values(self):
         doc = {'k': [{'kk': 'v1'}, {'kk': 'v2'}]}
         self.assertEqual(['v1', 'v2'],
-                         list(_select_value('k.kk', doc)))
+                         list(_retrieve_doc_values('k.kk', doc)))
 
 
 class TestSelectorValueMatcher(unittest.TestCase):
@@ -168,52 +168,52 @@ class TestSelectorValueMatcher(unittest.TestCase):
 
 class TestSelectorCreatePredicate(unittest.TestCase):
     def test_empty_selector_match_anything(self):
-        pred = _create_predicate_from_selector({})
+        pred = _create_pred_from_selector({})
         self.assertTrue(pred({}))
         self.assertTrue(pred({'k': 'v'}))
         
     def test_simple_1item_selector_match(self):
-        pred = _create_predicate_from_selector({'k1': 'v1'})
+        pred = _create_pred_from_selector({'k1': 'v1'})
         self.assertTrue(pred({'k1': 'v1'}))
         self.assertTrue(pred({'k1': 'v1', 'k2': 'v2'}))
     
     def test_simple_1item_selector_nomatch(self):
-        pred = _create_predicate_from_selector({'k1': 'v1'})
+        pred = _create_pred_from_selector({'k1': 'v1'})
         self.assertFalse(pred({}))
         self.assertFalse(pred({'k2': 'v2'}))
         self.assertFalse(pred({'k1': 'v2'}))
     
     def test_simple_2item_selector_match(self):
-        pred = _create_predicate_from_selector({'k1': 'v1', 'k2': 'v2'})
+        pred = _create_pred_from_selector({'k1': 'v1', 'k2': 'v2'})
         self.assertTrue(pred({'k1': 'v1', 'k2': 'v2'}))
         self.assertTrue(pred({'k1': 'v1', 'k2': 'v2', 'k3': 'v3'}))
         
     def test_simple_2item_selector_nomatch(self):
-        pred = _create_predicate_from_selector({'k1': 'v1', 'k2': 'v2'})
+        pred = _create_pred_from_selector({'k1': 'v1', 'k2': 'v2'})
         self.assertFalse(pred({}))
         self.assertFalse(pred({'k1': 'v1'}))
         self.assertFalse(pred({'k2': 'v2'}))
         self.assertFalse(pred({'k1': 'v1', 'k2': 'v1'}))
 
     def test_1item_list_selector_match(self):
-        pred = _create_predicate_from_selector({'k1': 'v1'})
+        pred = _create_pred_from_selector({'k1': 'v1'})
         self.assertTrue(pred({'k1': ['v1']}))
         self.assertTrue(pred({'k1': ['v2', 'v1']}))
         self.assertTrue(pred({'k1': 'v1'}))
 
     def test_1item_list_selector_nomatch(self):
-        pred = _create_predicate_from_selector({'k1': 'v1'})
+        pred = _create_pred_from_selector({'k1': 'v1'})
         self.assertFalse(pred({'k1': ['v2']}))
         self.assertFalse(pred({'k1': 'v2'}))
     
     def test_1item_dict_selector_match(self):
-        pred = _create_predicate_from_selector({'k.kk': 'v'})
+        pred = _create_pred_from_selector({'k.kk': 'v'})
         self.assertTrue(pred({'k': {'kk': 'v'}}))
         self.assertTrue(pred({'k': {'kk': 'v', 'foo': 'bar'}}))
         self.assertTrue(pred({'k': [{'kk': 'v'}]}))
     
     def test_1item_dict_selector_nomatch(self):
-        pred = _create_predicate_from_selector({'k.kk': 'v'})
+        pred = _create_pred_from_selector({'k.kk': 'v'})
         self.assertFalse(pred({'k': {'kk': 'v1'}}))
         self.assertFalse(pred({'k': 'v'}))
         self.assertFalse(pred({'k': {'foo': 'bar'}}))
