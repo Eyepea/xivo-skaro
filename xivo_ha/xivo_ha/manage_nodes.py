a21acfe75347f9a8353ae720e34ce790d69c9dbe
Nicolas Hicher <nhicher@proformatique.com>
2011-05-25 09:53:41 +0000

    typo in pf-xivo-ha.conf, add licenses gplv3
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@10867 dc85baef-7018-0410-bfbd-8a639da648bb

--- xivo_ha/xivo_ha/manage_nodes.py
+++ xivo_ha/xivo_ha/manage_nodes.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
+__author__  = "Nicolas HICHER <nhicher@proformatique.com>"
+__license__ = """
+    Copyright (C) 2011  Proformatique <technique@proformatique.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 import os
 import re
 import sys
@@ -172,6 +189,9 @@ class ManageService(object):
                 return state.stderr.readline()
         except:
             raise IOError("impossible to disable service %s" % self.service_name)
+    
+    def _stop_service(self):
+        raise NotImplementedError
 
     def initialize(self):
         if self._is_available():
