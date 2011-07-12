7f2a32646a1522a2a4d3de5868275fb4caad589c
Guillaume Bour <gbour@proformatique.com>
2011-07-12 14:46:04 +0000

    [evol] ticket #2145: add webservice to get xivo uuid
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@11257 dc85baef-7018-0410-bfbd-8a639da648bb

---
+++ sysconfd/unittest/test04_xivo.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+__version__ = "$Revision$ $Date$"
+__author__  = "Guillaume Bour <gbour@proformatique.com>"
+__license__ = """
+    Copyright (C) 2010-2011 Proformatique, Guillaume Bour <g.bour@proformatique.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA..
+"""
+import unittest, cjson, pprint
+import sysconfd_client
+
+class TestXivo(unittest.TestCase):
+	def setUp(self):
+		self.client = sysconfd_client.SysconfdClient()
+
+	def test_01_uuid(self):
+		#NOTE: we suppose test is executed locally
+		with open('/usr/share/pf-xivo/XIVO-UUID') as f:
+			uuid = f.read()[:-1]
+
+		(resp, data) = self.client.request('GET', '/xivo_uuid', {})
+		self.assertEqual(resp.status, 200)
+
+		data = cjson.decode(data)
+		self.assertTrue('uuid' in data)
+		self.assertEqual(data['uuid'], uuid)
+ 
+
+if __name__ == '__main__':
+	unittest.main()
