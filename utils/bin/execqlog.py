c68307baefd281ec869cfd5f86b0a283ff9dd7c6
Cédric Abunar <cabunar@proformatique.com>
2011-07-19 14:10:02 +0000

    add recovery of the identity of agents refactoring code
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@11295 dc85baef-7018-0410-bfbd-8a639da648bb

--- utils/bin/execqlog.py
+++ utils/bin/execqlog.py
@@ -19,21 +19,23 @@ __author__    = 'Cedric Abunar'
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os, subprocess, traceback
-import sys
-import urllib2
-import psycopg2
-import json
-import fnmatch
-import time
-import gzip
-import contextlib
-import re
 import ConfigParser
-
-from sys import exit, stderr
+import contextlib
+import fnmatch
+import gzip
+import json
+import os
+import re
+import subprocess
+import sys
+import time
+import traceback
+import urllib2
 from optparse import OptionParser
+from sys import exit, stderr
 from urllib2 import URLError, HTTPError
+
+import psycopg2
 from xivo import anysql
 from xivo.BackSQL import backpostgresql
 
@@ -41,13 +43,14 @@ from xivo.BackSQL import backpostgresql
 # Define variables
 
 VERBOSE = False
+DEBUG = False
 IP_XIVO = "127.0.0.1"
 FILE_DELIMITERS = '|'
 DEFAULT_OUT_FILE = "/tmp/insert_file_pg_copy_%s" % time.strftime('%Y%m%d%H%M%S',time.localtime())
-DEFAULT_DB_NAME = 'asterisk'
 
-IPBX_INI_FILE = os.path.join(os.path.sep, 'etc', 'pf-xivo','web-interface','ipbx.ini')
-QLOG_DIR = os.path.join(os.path.sep, 'var','lib','pf-xivo-web-interface','statistics','qlog')
+CONF_INI_FILE = '/etc/pf-xivo/stats_accounts.ini'
+IPBX_INI_FILE = '/etc/pf-xivo/web-interface/ipbx.ini'
+QLOG_DIR = '/var/lib/pf-xivo-web-interface/statistics/qlog'
 
 URL_QUEUE = "https://%s/callcenter/json.php/private/settings/queues/?forcedatabase=%s"
 URL_AGENT = "https://%s/callcenter/json.php/private/settings/agents/?forcedatabase=%s"
@@ -73,7 +76,7 @@ DATA_QUEUE ={ "queuefeatures":
 DATA_AGENT ={ "agentfeatures": 
                 {
                     "firstname": "%s", 
-                    "lastname": "", 
+                    "lastname": "%s", 
                     "context": "default", 
                     "number": "%s", 
                     "numgroup": 1, 
@@ -91,10 +94,28 @@ DATA_AGENT ={ "agentfeatures":
 
 COMMAND_COPY = "COPY queue_log (time,callid,queuename,agent,event,data1,data2,data3,data4,data5) " \
                "FROM '%s' DELIMITERS '%s' CSV"  %( DEFAULT_OUT_FILE, FILE_DELIMITERS)
+               
 
 # end of define
 ##############################################
 
+
+def parse_qlog_dir_process():
+    global DB_NAME
+    global CLIENT
+    config = ConfigParser.RawConfigParser()
+    with open(CONF_INI_FILE) as fobj:
+        config.readfp(fobj)
+    list_client = config.sections()
+    for client in list_client:
+        dir_process = os.path.join(QLOG_DIR, client, 'process')
+        if os.access(dir_process, os.R_OK):
+            if VERBOSE:
+                print 'Processing dirrectory for', client, '...'
+            DB_NAME = 'ast_%s' % client
+            CLIENT = client
+            main(dir_process)
+    
 
 class DbManager(object):
     
@@ -131,14 +152,15 @@ def _open_file(file):
 
 
 #@_limit(50000)
-def readlines(directory):
+def readlines(directory, backup=None):
     for file in os.listdir(directory):
         abs_file = os.path.join(directory, file)
         with _open_file(abs_file) as fobj:
             for line in fobj:
                 yield line
-        newfile = os.path.join(QLOG_DIR, CLIENT_NAME, 'backup', file)
-        os.rename(abs_file, newfile)
+        if backup is not None:
+            newfile = os.path.join(QLOG_DIR, CLIENT, 'backup', file)
+            os.rename(abs_file, newfile)
 
 
 def traitmentline(line, separator='|'):
@@ -146,6 +168,18 @@ def traitmentline(line, separator='|'):
     while len(rs) <= 10:
         rs.append('')
     return rs[:10]
+
+
+def find_most_recent(directory, partial_file_name=None): 
+    # list all the files in the directory
+    files = os.listdir(directory) 
+    # remove all file names that don't match partial_file_name string
+    if partial_file_name is not None:
+        files = filter(lambda x: x.find(partial_file_name) > -1, files) 
+    # create a dict that contains list of files and their modification timestamps
+    name_n_timestamp = dict([(x, os.stat(os.path.join(directory,x)).st_mtime) for x in files]) 
+    # return the file with the latest timestamp
+    return max(name_n_timestamp, key=lambda k: name_n_timestamp.get(k))
 
 
 def valid_line(cursor, linet):
@@ -168,41 +202,55 @@ def request_http(url, data=None):
         f = urllib2.urlopen(req) 
         return f
     except HTTPError, e:
-        print 'code:', e.code,' response:', e.read()
+        if DEBUG:
+            print 'code:', e.code,' response:', e.read()
     except URLError, e:
-        print 'code:', e.code,' response:', e.read()
+        if DEBUG:
+            print 'code:', e.code,' response:', e.read()
 
 
 def exec_ws():
-    base_url_queue = URL_QUEUE % (IP_XIVO, DEFAULT_DB_NAME)
-    base_url_agent = URL_AGENT % (IP_XIVO, DEFAULT_DB_NAME)
+    base_url_queue = URL_QUEUE % (IP_XIVO, DB_NAME)
     for q in LIST_QUEUE:
         f = request_http('%s&act=search&search=%s' % (base_url_queue, q))
         if hasattr(f, 'code'):
-            if VERBOSE:
+            if DEBUG:
                 print 'code:', f.code,' response:', f.read()
             if f.code == 204:
                 data = json.dumps(DATA_QUEUE) % q
                 f = request_http('%s&act=add' % (base_url_queue), data)
-                if VERBOSE:
-                    print 'JSON:', data
+                if DEBUG:
+                    print 'QUEUE ADD JSON:', data
                     #print 'code:', f.code,' response:', f.read()
+    base_url_agent = URL_AGENT % (IP_XIVO, DB_NAME)
     for a in LIST_AGENT:
         m = re.search("agent/([0-9]+)", a, re.I)
         if m is not None:
-            data = json.dumps(DATA_AGENT) % (a, m.group(1))
+            agent_dir = os.path.join(QLOG_DIR, CLIENT, 'agent');
+            number = m.group(1)
+            agent_filename = find_most_recent(agent_dir)
+            agent_filepath = os.path.join(agent_dir, agent_filename)
+            agents = json.load(open(agent_filepath))
+            if agents:
+                for agent in agents:
+                    if number == agent[5]:
+                        firstname = agent[3]
+                        lastname = agent[4]
+                        if VERBOSE:
+                            print 'Found agent identity for', a, ':', firstname, lastname
+            data = json.dumps(DATA_AGENT) % (firstname, lastname, number)
             f = request_http('%s&act=add' % (base_url_agent), data)
-            if VERBOSE:
-                print 'JSON:', data
+            if DEBUG:
+                print 'AGENT ADD JSON:', data
                 #print 'code:', f.code,' response:', f.read()
-        
+
 
 def main(directory):
     if not os.access(directory, os.R_OK):
         raise Exeption("Can't open directory %s" % directory)
     dbman = DbManager()
     fobj = open(DEFAULT_OUT_FILE,'w')
-    for line in readlines(directory):
+    for line in readlines(directory,True):
         linet = traitmentline(line)
         if valid_line(dbman.cursor, linet):
             datetime = time.strftime('%Y-%m-%d %H:%M:%S',time.gmtime((int(linet[0]))))
@@ -215,7 +263,7 @@ def main(directory):
     fobj.close()
     dbman.close()
     
-    PGCREATEDB = "CREATE DATABASE \"%s\" TEMPLATE tpl_asterisk;" % DEFAULT_DB_NAME
+    PGCREATEDB = "CREATE DATABASE \"%s\" TEMPLATE tpl_asterisk;" % DB_NAME
     
     try:
         if VERBOSE:
@@ -228,8 +276,8 @@ def main(directory):
     
     try:
         if VERBOSE:
-            print 'sudo', '-H', '-u', 'postgres' ,'psql' ,DEFAULT_DB_NAME, '-c', COMMAND_COPY
-        subprocess.check_call(['sudo', '-H', '-u', 'postgres' ,'psql' ,DEFAULT_DB_NAME, '-c', COMMAND_COPY])
+            print 'sudo', '-H', '-u', 'postgres' ,'psql' ,DB_NAME, '-c', COMMAND_COPY
+        subprocess.check_call(['sudo', '-H', '-u', 'postgres' ,'psql' ,DB_NAME, '-c', COMMAND_COPY])
     except (OSError, subprocess.CalledProcessError), e:
         traceback.print_exc()
 
@@ -238,20 +286,25 @@ if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option('-v', '--verbose', action='store_true', dest='verbose',
                       help='mode verbose')
+    parser.add_option('-d', '--debug', action='store_true', dest='debug',
+                      help='mode debug')
     parser.add_option('-o', '--outfile', action='store', dest='output_filename',
                       help='choose out file', default=DEFAULT_OUT_FILE)
-    parser.add_option('-d', '--dbname', action='store', dest='output_db_name',
-                      help='choose db name', default=DEFAULT_DB_NAME)
+    parser.add_option('-c', '--client', action='store', dest='output_client',
+                      help='choose client')
     opts, args = parser.parse_args()
     if opts.verbose:
         VERBOSE = True
+    if opts.debug:
+        DEBUG = True
     if opts.output_filename:
         DEFAULT_OUT_FILE = opts.output_filename
-    if opts.output_db_name:
-        DEFAULT_DB_NAME = 'ast_%s' % opts.output_db_name
-        CLIENT_NAME = opts.output_db_name
+    if opts.output_client:
+        DB_NAME = 'ast_%s' % opts.output_client
+        CLIENT = opts.output_client
+        main(os.path.join(QLOG_DIR, CLIENT, 'process'))
     else:
-        print >>sys.stderr, "usage: %s -v -d <client_name> [-o <output_filename>]" % ("./execqlog.py")
-        raise sys.exit(1)
-    main(os.path.join(os.path.sep, QLOG_DIR, CLIENT_NAME, 'process'))
+        parse_qlog_dir_process()
+    #    print >>sys.stderr, "usage: %s -c <client_name>" % ("./execqlog.py")
+    #    raise sys.exit(1)
 
