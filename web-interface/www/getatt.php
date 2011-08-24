98e497ec4533a152c0d5a22be82ef3fbb0a0d8d8
Cedric Abunar <cabunar@proformatique.com>
2011-08-24 10:54:02 -0400

    fix attachment module for postgresql-9.0 set bytea_output before query

--- web-interface/www/getatt.php
+++ web-interface/www/getatt.php
@@ -25,52 +25,47 @@ $ipbx = &$_SRE->get('ipbx');
 dwho::load_class('dwho_http');
 $http_response = dwho_http::factory('response');
 
-$modattachment = &$ipbx->get_module('attachment');
-
 $act = isset($_QR['act']) === false ? null : $_QR['act'];
 $obj = isset($_QR['obj']) === false ? null : $_QR['obj'];
 $id = isset($_QR['id']) === false ? null : (int) $_QR['id'];
 
+$modattachment = &$ipbx->get_module('attachment');
+
 switch ($obj)
 {
-    case null:
-        if(($rs = $modattachment->get($id)) === false)
-        {
-            $http_response->set_status_line(400);
-            $http_response->send(true);
-        }
-        break;
-    default:
-        $arr = array();
-        $arr['object_type'] = $obj;
-        $arr['object_id'] = $id;
-        if(($rs = $modattachment->get_where($arr)) === false)
-        {
-            $http_response->set_status_line(400);
-            $http_response->send(true);
-        }
+	case null:
+		if(($rs = $modattachment->get($id)) === false)
+		{
+			$http_response->set_status_line(400);
+			$http_response->send(true);
+		}
+		break;
+	default:
+		$arr = array();
+		$arr['object_type'] = $obj;
+		$arr['object_id'] = $id;
+		if(($rs = $modattachment->get_where($arr)) === false)
+		{
+			$http_response->set_status_line(400);
+			$http_response->send(true);
+		}
 }
 
 switch ($act)
 {
-    case 'download':
-        header("Pragma: public"); // required 
-        header("Expires: 0"); 
-        header("Cache-Control: must-revalidate, post-check=0, pre-check=0"); 
-        header("Cache-Control: private",false); // required for certain browsers 
-        header("Content-Type: ".$rs['mime']); 
-        header("Content-Disposition: attachment; filename=\"".$rs['name']."\";" ); 
-        header("Content-Transfer-Encoding: binary"); 
-        header("Content-Length: ".$rs['size']);
-        break;
-    case null:
-    default: 
-        header("Content-Type: ".$rs['mime']); 
+	case 'download':
+		header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+		header("Cache-Control: private",false); // required for certain browsers
+		header("Content-Type: ".$rs['mime']);
+		header("Content-Disposition: attachment; filename=\"".$rs['name']."\";" );
+		header("Content-Transfer-Encoding: binary");
+		header("Content-Length: ".$rs['size']);
+		break;
+	case null:
+	default:
+		header("Content-Type: ".$rs['mime']);
 }
 
-ob_clean(); 
-flush();
-echo $rs['file'];
-die();
+die($rs['file']);
 
 ?>
