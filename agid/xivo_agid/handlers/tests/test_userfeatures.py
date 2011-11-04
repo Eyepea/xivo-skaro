c52a6798a5b7af5dc06113b27adbc750ad8e22e5
Pascal Cadotte-Michaud <pcadottemichaud@proformatique.com>
2011-11-04 09:21:31 -0400

    Start refactoring incoming_user_set_features

---
+++ agid/xivo_agid/handlers/tests/test_userfeatures.py
@@ -0,0 +1,91 @@
+import unittest
+from mock import Mock, patch
+
+from xivo_agid.handlers.userfeatures import UserFeatures
+from xivo_agid import objects
+
+
+class TestUserFeatures(unittest.TestCase):
+
+    def setUp(self):
+        self._variables = {'XIVO_USERID': 42,
+                           'XIVO_DSTID': 33,
+                           'XIVO_LINEID': 5,
+                           'XIVO_CALLORIGIN' : 'my_origin',
+                           'XIVO_CALLFILTER_BYPASS': 'my_filter',
+                           'XIVO_SRCNUM': '1000',
+                           'XIVO_DSTNUM': '1003',}
+
+        def get_variable(key):
+            return self._variables[key]
+
+        self._agi = Mock()
+        self._agi.get_variable = get_variable
+        self._cursor = Mock()
+        self._args = Mock()
+        self._old_extenfeatures = objects.ExtenFeatures
+        objects.ExtenFeatures = Mock
+
+    def tearDown(self):
+        objects.ExtenFeatures = self._old_extenfeatures
+        self._old_extenfeatures = None
+
+
+    def test_userfeatures(self):
+        userfeatures = UserFeatures(self._agi, self._cursor, self._args)
+
+        self.assertEqual(userfeatures._agi, self._agi)
+        self.assertEqual(userfeatures._cursor, self._cursor)
+        self.assertEqual(userfeatures._args, self._args)
+
+    def test_set_members(self):
+        userfeatures = UserFeatures(self._agi, self._cursor, self._args)
+        userfeatures._set_feature_list = Mock()
+        userfeatures._set_caller = Mock()
+
+        userfeatures.set_members()
+
+        objects.ExtenFeatures = Mock()
+        objects.User = Mock()
+
+        self.assertEqual(userfeatures._userid, self._variables['XIVO_USERID'])
+        self.assertEqual(userfeatures._dstid, self._variables['XIVO_DSTID'])
+        self.assertEqual(userfeatures._lineid, self._variables['XIVO_LINEID'])
+        self.assertEqual(userfeatures._zone, self._variables['XIVO_CALLORIGIN'])
+        self.assertEqual(userfeatures._bypass_filter, self._variables['XIVO_CALLFILTER_BYPASS'])
+        self.assertEqual(userfeatures._srcnum, self._variables['XIVO_SRCNUM'])
+        self.assertEqual(userfeatures._dstnum, self._variables['XIVO_DSTNUM'])
+        self.assertTrue(userfeatures._set_feature_list.called)
+        self.assertTrue(userfeatures._set_caller.called)
+
+    def test_set_feature_list(self):
+        userfeatures = UserFeatures(self._agi, self._cursor, self._args)
+        
+        with patch.object(objects.ExtenFeatures, '__init__') as extenfeatures_init:
+            extenfeatures_init.return_value = None
+            userfeatures._set_feature_list()
+            extenfeatures_init.assert_called_with(self._agi, self._cursor)
+        self.assertNotEqual(userfeatures._feature_list, None)
+        self.assertTrue(isinstance(userfeatures._feature_list, objects.ExtenFeatures))
+
+    def test_set_caller(self):
+        userfeatures = UserFeatures(self._agi, self._cursor, self._args)
+
+        userfeatures._set_caller()
+
+        self.assertTrue(userfeatures._caller is None)
+
+        userfeatures._userid = self._variables['XIVO_USERID']
+
+        with patch.object(objects.User, '__init__') as user_init:
+            user_init.return_value = None
+            userfeatures._set_caller()
+            user_init.assert_called_with(self._agi, self._cursor, self._variables['XIVO_USERID'])
+
+        self.assertTrue(userfeatures._caller is not None)
+        self.assertTrue(isinstance(userfeatures._caller, objects.User))
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.test_']
+    unittest.main()
---
+++ agid/xivo_agid/handlers/userfeatures.py
@@ -0,0 +1,39 @@
+# -*- coding: UTF-8 -*-
+from xivo_agid.handlers.Handler import Handler
+from xivo_agid import objects
+
+
+class UserFeatures(Handler):
+
+    def __init__(self, agi, cursor, args):
+        Handler.__init__(self, agi, cursor, args)
+        self._userid = None
+        self._dstid = None
+        self._lineid = None
+        self._zone = None
+        self._bypass_filter = None
+        self._srcnum = None
+        self._dstnum = None
+        self._feature_list = None
+        self._caller = None
+
+    def set_members(self):
+        self._userid = self._agi.get_variable('XIVO_USERID')
+        self._dstid = self._agi.get_variable('XIVO_DSTID')
+        self._lineid = self._agi.get_variable('XIVO_LINEID')
+        self._zone = self._agi.get_variable('XIVO_CALLORIGIN')
+        self._bypass_filter = self._agi.get_variable('XIVO_CALLFILTER_BYPASS')
+        self._srcnum = self._agi.get_variable('XIVO_SRCNUM')
+        self._dstnum = self._agi.get_variable('XIVO_DSTNUM')
+        self._set_feature_list()
+        self._set_caller()
+
+    def _set_feature_list(self):
+        self._feature_list = objects.ExtenFeatures(self._agi, self._cursor)
+
+    def _set_caller(self):
+        if self._userid:
+            try:
+                self._caller = objects.User(self._agi, self._cursor, int(self._userid))
+            except (ValueError, LookupError):
+                self._caller = None
