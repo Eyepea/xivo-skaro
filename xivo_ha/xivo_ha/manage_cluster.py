7f8c689c49a715a032d13fbc7b186809567f3aa8
Etienne Lessard <edesrocherslessard@avencall.com>
2011-11-23 10:35:59 -0500

    Update copyright from Proformatique to Avencall in xivo_ha.

--- xivo_ha/xivo_ha/manage_cluster.py
+++ xivo_ha/xivo_ha/manage_cluster.py
@@ -2,7 +2,7 @@
 # -*- coding: utf8 -*-
 __author__  = "Nicolas HICHER <nhicher@proformatique.com>, Guillaume Bour <gbour@proformatique.com>"
 __license__ = """
-    Copyright (C) 2011  Proformatique <technique@proformatique.com>
+    Copyright (C) 2011  Avencall
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
