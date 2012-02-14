26bdaa24ec30ea1b762b01ea4b24ce8277c7c821
Pascal Cadotte-Michaud <pcadottemichaud@proformatique.com>
2012-02-14 09:32:01 -0500

    [refactor] white spaces

--- agid/xivo_agid/handlers/outgoingfeatures.py
+++ agid/xivo_agid/handlers/outgoingfeatures.py
@@ -22,10 +22,11 @@ from xivo_agid.handlers.Handler import Handler
 from xivo_agid import objects
 import time
 
+
 class OutgoingFeatures(Handler):
-    
+
     PATH_TYPE = 'outcall'
-    
+
     def __init__(self, agi, cursor, args):
         Handler.__init__(self, agi, cursor, args)
         self.userid = None
@@ -33,7 +34,6 @@ class OutgoingFeatures(Handler):
         self.callrecord = False
         self.options = ""
         self.outcall = objects.Outcall(self._agi, self._cursor)
-    
 
     def _retrieve_outcall(self):
         try:
@@ -41,20 +41,18 @@ class OutgoingFeatures(Handler):
         except (ValueError, LookupError) as e:
             self._agi.dp_break(str(e))
 
-
     def _set_destination_number(self):
         if self.outcall.stripnum > 0:
             self.dstnum = self.dstnum[self.outcall.stripnum:]
         if self.outcall.externprefix:
             self.dstnum = self.outcall.externprefix + self.dstnum
 
-
     def _retrieve_user(self):
         try:
             self.user = objects.User(self._agi, self._cursor, int(self.userid))
             if self.user.enablexfer:
                 self.options += 'T'
-                
+
             if not self.outcall.internal:
                 self.callerid = self.user.outcallerid
                 if self.user.enableautomon:
@@ -64,7 +62,6 @@ class OutgoingFeatures(Handler):
             pass
         self._agi.set_variable(dialplan_variables.CALL_OPTIONS, self.options)
 
-
     def _set_caller_id(self):
         if not self.outcall.internal:
             if self.callerid in (None, '', 'default') and self.outcall.callerid:
@@ -73,7 +70,6 @@ class OutgoingFeatures(Handler):
             objects.CallerID.set(self._agi, self.callerid)
             if self.callerid == 'anonymous':
                 self._agi.appexec('SetCallerPres', 'prohib')
-
 
     def _set_trunk_info(self):
         for i, trunk in enumerate(self.outcall.trunks):
@@ -85,14 +81,12 @@ class OutgoingFeatures(Handler):
                 intfsuffix = ""
             self._agi.set_variable('%s%d' % (dialplan_variables.TRUNK_SUFFIX, i), intfsuffix)
 
-
     def _set_record_file_name(self):
-        if self.callrecord and objects.ExtenFeatures(self._agi, self._cursor).callrecord: # BUGBUG the context is missing in the filename TODO use ids
+        if self.callrecord and objects.ExtenFeatures(self._agi, self._cursor).callrecord:  # BUGBUG the context is missing in the filename TODO use ids
             callrecordfile = "user-%s-%s-%s.wav" % (self.srcnum, self.orig_dstnum, int(time.time()))
         else:
             callrecordfile = ""
         self._agi.set_variable(dialplan_variables.CALL_RECORD_FILE_NAME, callrecordfile)
-
 
     def _set_preprocess_subroutine(self):
         if self.outcall.preprocess_subroutine:
@@ -101,15 +95,12 @@ class OutgoingFeatures(Handler):
             preprocess_subroutine = ""
         self._agi.set_variable(dialplan_variables.OUTCALL_PREPROCESS_SUBROUTINE, preprocess_subroutine)
 
-
     def _set_hangup_ring_time(self):
         if self.outcall.hangupringtime:
             hangupringtime = self.outcall.hangupringtime
         else:
             hangupringtime = ""
         self._agi.set_variable(dialplan_variables.HANGUP_RING_TIME, hangupringtime)
-
-
 
     def _extract_dialplan_variables(self):
         self.userid = self._agi.get_variable(dialplan_variables.USERID)
