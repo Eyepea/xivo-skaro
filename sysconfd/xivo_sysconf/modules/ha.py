dfc5f092484f1831185823290601a3f50719f005
Etienne Lessard <elessard@avencall.com>
2012-03-02 15:49:50 -0500

    Add postgres configuration files management for HA in sysconfd.

--- sysconfd/xivo_sysconf/modules/ha.py
+++ sysconfd/xivo_sysconf/modules/ha.py
@@ -20,48 +20,104 @@ __license__ = """
 
 import errno
 import json
+import os
+import subprocess
 from xivo import http_json_server
 from xivo.http_json_server import CMD_R, CMD_RW
 
-HA_CONF_FILE = '/etc/pf-xivo/ha.conf'
-DEFAULT_HA_CONFIG = {
-    "node_type": "disabled",
-    "remote_address": ""
-}
+
+class HAConfigManager(object):
+    DEFAULT_HA_CONF_FILE = '/etc/pf-xivo/ha.conf'
+    DEFAULT_HA_CONFIG = {
+        'node_type': 'disabled',
+        'remote_address': ''
+    }
+
+    def __init__(self, postgres_config_updater_factory, ha_conf_file=DEFAULT_HA_CONF_FILE):
+        self._postgres_config_updater_factory = postgres_config_updater_factory
+        self._ha_conf_file = ha_conf_file
+
+    def get_ha_config(self, args, options):
+        return self._read_ha_config()
+
+    def _read_ha_config(self):
+        try:
+            with open(self._ha_conf_file) as fobj:
+                return self._read_ha_config_from_fobj(fobj)
+        except IOError as e:
+            if e.errno == errno.ENOENT:
+                return dict(self.DEFAULT_HA_CONFIG)
+            else:
+                raise
+
+    def _read_ha_config_from_fobj(self, fobj):
+        return json.load(fobj)
+
+    def update_ha_config(self, args, options):
+        ha_config = args
+        self._write_ha_config(ha_config)
+        self._update_postgres(ha_config)
+
+    def _write_ha_config(self, ha_config):
+        with open(self._ha_conf_file, 'wb') as fobj:
+            self._write_ha_config_to_fobj(ha_config, fobj)
+
+    def _write_ha_config_to_fobj(self, ha_config, fobj):
+        json.dump(ha_config, fobj)
+
+    def _update_postgres(self, ha_config):
+        postgres_updater = self._postgres_config_updater_factory(ha_config)
+        postgres_updater.update_pg_hba_file()
+        postgres_updater.update_postgresql_file()
+        postgres_updater.restart_postgres()
 
 
-def get_ha_config(args, options):
-    return _read_ha_config()
+class _PostgresConfigUpdater(object):
+    DEFAULT_CONFIG_DIR = '/etc/postgresql/9.0/main'
+    PG_HBA_FILE = 'pg_hba.conf'
+    POSTGRESQL_FILE = 'postgresql.conf'
+
+    def __init__(self, ha_config, postgres_config_dir=DEFAULT_CONFIG_DIR):
+        self._ha_config = ha_config
+        self._pg_hba_file = os.path.join(postgres_config_dir, self.PG_HBA_FILE)
+        self._postgresql_file = os.path.join(postgres_config_dir, self.POSTGRESQL_FILE)
+
+    def update_pg_hba_file(self):
+        self._clear_host_line_in_pg_hba()
+        if self._ha_config['node_type'] == 'slave':
+            self._append_host_line_in_pg_hba()
+
+    def _clear_host_line_in_pg_hba(self):
+        command_args = ['sed', '-i', '/^host asterisk postgres/d', self._pg_hba_file]
+        subprocess.check_call(command_args, close_fds=True)
+
+    def _append_host_line_in_pg_hba(self):
+        master_ip_address = self._ha_config['remote_address']
+        host_line = 'host asterisk postgres %s/32 trust\n' % master_ip_address
+        with open(self._pg_hba_file, 'a') as fobj:
+            fobj.write(host_line)
+
+    def update_postgresql_file(self):
+        self._clear_listen_addresses_line_in_postgresql()
+        if self._ha_config['node_type'] == 'slave':
+            self._append_listen_addresses_line_in_postgresql()
+
+    def _clear_listen_addresses_line_in_postgresql(self):
+        command_args = ['sed', '-i', '/^listen_addresses/d', self._postgresql_file]
+        subprocess.check_call(command_args, close_fds=True)
+
+    def _append_listen_addresses_line_in_postgresql(self):
+        listen_addresses_line = "listen_addresses = '*'\n"
+        with open(self._postgresql_file, 'a') as fobj:
+            fobj.write(listen_addresses_line)
+
+    def restart_postgres(self):
+        command_args = ['/etc/init.d/postgresql', 'restart']
+        subprocess.check_call(command_args, close_fds=True)
 
 
-def _read_ha_config():
-    try:
-        with open(HA_CONF_FILE) as fobj:
-            return _read_ha_config_from_fobj(fobj)
-    except IOError as e:
-        if e.errno == errno.ENOENT:
-            return dict(DEFAULT_HA_CONFIG)
-        else:
-            raise
-
-
-def _read_ha_config_from_fobj(fobj):
-    return json.load(fobj)
-
-
-def update_ha_config(args, options):
-    ha_config = args
-    _write_ha_config(ha_config)
-
-
-def _write_ha_config(ha_config):
-    with open(HA_CONF_FILE, 'wb') as fobj:
-        _write_ha_config_to_fobj(ha_config, fobj)
-
-
-def _write_ha_config_to_fobj(ha_config, fobj):
-    json.dump(ha_config, fobj)
-
-
-http_json_server.register(get_ha_config, CMD_R, name='get_ha_config')
-http_json_server.register(update_ha_config, CMD_RW, name='update_ha_config')
+ha_config_manager = HAConfigManager(_PostgresConfigUpdater)
+http_json_server.register(ha_config_manager.get_ha_config, CMD_R,
+                          name='get_ha_config')
+http_json_server.register(ha_config_manager.update_ha_config, CMD_RW,
+                          name='update_ha_config')
--- sysconfd/xivo_sysconf/modules/tests/test_ha.py
+++ sysconfd/xivo_sysconf/modules/tests/test_ha.py
@@ -18,90 +18,238 @@ __license__ = """
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA..
 """
 
+import mock
 import os.path
 import shutil
 import tempfile
 import unittest
-
-import xivo_sysconf.modules.ha as ha
 from StringIO import StringIO
+from xivo_sysconf.modules.ha import HAConfigManager, _PostgresConfigUpdater
+
+
+def new_master_ha_config(slave_ip_address):
+    return _new_ha_config('master', slave_ip_address)
+
+
+def new_slave_ha_config(master_ip_address):
+    return _new_ha_config('slave', master_ip_address)
+
+
+def new_disabled_ha_config():
+    return _new_ha_config('disabled', '')
+
+
+def _new_ha_config(node_type, remote_address):
+    return {'node_type': node_type, 'remote_address': remote_address}
+
 
 class TestHA(unittest.TestCase):
     def setUp(self):
-        self.old_ha_conf_file = ha.HA_CONF_FILE
         self._tmp_dir = tempfile.mkdtemp()
-        ha.HA_CONF_FILE = os.path.join(self._tmp_dir, 'test_ha.conf')
+        self._ha_conf_file = os.path.join(self._tmp_dir, 'test_ha.conf')
+        self._ha_config_mgr = HAConfigManager(mock.Mock(), self._ha_conf_file)
 
     def tearDown(self):
-        ha.HA_CONF_FILE = self.old_ha_conf_file
         shutil.rmtree(self._tmp_dir)
 
     def _create_tmp_conf_file(self, content):
-        with open(ha.HA_CONF_FILE, 'wb') as fobj:
+        with open(self._ha_conf_file, 'wb') as fobj:
             fobj.write(content)
 
     def test_read_conf(self):
-        content = \
-        """{
-            "node_type" : "master",
-            "remote_address" : "10.0.0.1"
-        }"""
-        expected_ha_config = {
-            "node_type" : "master",
-            "remote_address" : "10.0.0.1"
-        }
+        content = """\
+{
+    "node_type" : "master",
+    "remote_address" : "10.0.0.1"
+}
+"""
+        expected_ha_config = new_master_ha_config('10.0.0.1')
         fobj = StringIO(content)
 
-        ha_config = ha._read_ha_config_from_fobj(fobj)
+        ha_config = self._ha_config_mgr._read_ha_config_from_fobj(fobj)
 
         self.assertEqual(expected_ha_config, ha_config)
 
     def test_write_conf(self):
-        ha_config = {
-            "node_type" : "master",
-            "remote_address" : "10.0.0.1"
-        }
+        ha_config = new_master_ha_config('10.0.0.1')
         expected_content = '{"node_type": "master", "remote_address": "10.0.0.1"}'
         fobj = StringIO()
 
-        ha._write_ha_config_to_fobj(ha_config, fobj)
+        self._ha_config_mgr._write_ha_config_to_fobj(ha_config, fobj)
 
         content = fobj.getvalue()
         self.assertEqual(expected_content, content)
 
     def test_get_ha_config_no_file(self):
-        expected_ha_config = {
-            "node_type" : "disabled",
-            "remote_address" : ""
-        }
+        expected_ha_config = new_disabled_ha_config()
 
-        ha_config = ha.get_ha_config(None, None)
+        ha_config = self._ha_config_mgr.get_ha_config(None, None)
 
         self.assertEqual(expected_ha_config, ha_config)
 
     def test_get_ha_config(self):
-        content = \
-        """{
-            "node_type" : "master",
-            "remote_address" : "10.0.0.1"
-        }"""
-        expected_ha_config = {
-            "node_type" : "master",
-            "remote_address" : "10.0.0.1"
-        }
+        content = """\
+{
+    "node_type" : "master",
+    "remote_address" : "10.0.0.1"
+}
+"""
+        expected_ha_config = new_master_ha_config('10.0.0.1')
         self._create_tmp_conf_file(content)
 
-        ha_config = ha.get_ha_config(None, None)
+        ha_config = self._ha_config_mgr.get_ha_config(None, None)
 
         self.assertEqual(expected_ha_config, ha_config)
 
     def test_update_ha_config(self):
-        ha_config = {
-            "node_type" : "master",
-            "remote_address" : "10.0.0.1"
-        }
+        ha_config = new_master_ha_config('10.0.0.1')
 
-        ha.update_ha_config(ha_config, None)
+        self._ha_config_mgr.update_ha_config(ha_config, None)
 
-        expected_ha_config = ha._read_ha_config()
+        expected_ha_config = self._ha_config_mgr._read_ha_config()
         self.assertEqual(expected_ha_config, ha_config)
+
+
+class TestPostgresConfigUpdater(unittest.TestCase):
+    def setUp(self):
+        self._tmp_dir = tempfile.mkdtemp()
+        self._pg_hba_filename = os.path.join(self._tmp_dir, _PostgresConfigUpdater.PG_HBA_FILE)
+        self._postgresql_filename = os.path.join(self._tmp_dir, _PostgresConfigUpdater.POSTGRESQL_FILE)
+
+    def tearDown(self):
+        shutil.rmtree(self._tmp_dir)
+
+    def test_update_pg_hba_file_from_non_slave_to_slave(self):
+        pg_hba_content = """\
+# PostgreSQL Client Authentication Configuration File
+local   all         postgres                          ident
+host    all             all             127.0.0.1/32            md5
+"""
+        expected_pg_hba_content = """\
+# PostgreSQL Client Authentication Configuration File
+local   all         postgres                          ident
+host    all             all             127.0.0.1/32            md5
+host asterisk postgres 10.0.0.1/32 trust
+"""
+        self._write_pg_hba_file(pg_hba_content)
+        ha_config = new_slave_ha_config('10.0.0.1')
+        postgres_updater = self._new_postgres_updater(ha_config)
+
+        postgres_updater.update_pg_hba_file()
+
+        self.assertEqual(expected_pg_hba_content, self._read_pg_hba_file())
+
+    def _new_postgres_updater(self, ha_config):
+        return _PostgresConfigUpdater(ha_config, self._tmp_dir)
+
+    def _write_pg_hba_file(self, content):
+        self._write_file(self._pg_hba_filename, content)
+
+    def _write_postgresql_file(self, content):
+        self._write_file(self._postgresql_filename, content)
+
+    def _write_file(self, filename, content):
+        with open(filename, 'w') as fobj:
+            fobj.write(content)
+
+    def _read_pg_hba_file(self):
+        return self._read_file(self._pg_hba_filename)
+
+    def _read_postgresql_file(self):
+        return self._read_file(self._postgresql_filename)
+
+    def _read_file(self, filename):
+        with open(filename) as fobj:
+            return fobj.read()
+
+    def test_update_pg_hba_file_from_slave_to_slave(self):
+        pg_hba_content = """\
+# PostgreSQL Client Authentication Configuration File
+local   all         postgres                          ident
+host    all             all             127.0.0.1/32            md5
+host asterisk postgres 10.0.0.1/32 trust
+"""
+        expected_pg_hba_content = """\
+# PostgreSQL Client Authentication Configuration File
+local   all         postgres                          ident
+host    all             all             127.0.0.1/32            md5
+host asterisk postgres 10.0.0.2/32 trust
+"""
+        self._write_pg_hba_file(pg_hba_content)
+        ha_config = new_slave_ha_config('10.0.0.2')
+        postgres_updater = self._new_postgres_updater(ha_config)
+
+        postgres_updater.update_pg_hba_file()
+
+        self.assertEqual(expected_pg_hba_content, self._read_pg_hba_file())
+
+    def test_update_pg_hba_file_from_slave_to_non_slave(self):
+        pg_hba_content = """\
+# PostgreSQL Client Authentication Configuration File
+local   all         postgres                          ident
+host    all             all             127.0.0.1/32            md5
+host asterisk postgres 10.0.0.1/32 trust
+"""
+        expected_pg_hba_content = """\
+# PostgreSQL Client Authentication Configuration File
+local   all         postgres                          ident
+host    all             all             127.0.0.1/32            md5
+"""
+        self._write_pg_hba_file(pg_hba_content)
+        ha_config = new_disabled_ha_config()
+        postgres_updater = self._new_postgres_updater(ha_config)
+
+        postgres_updater.update_pg_hba_file()
+
+        self.assertEqual(expected_pg_hba_content, self._read_pg_hba_file())
+
+    def test_update_postgresql_file_from_non_slave_to_slave(self):
+        postgresql_content = """\
+# PostgreSQL configuration file
+#listen_addresses = 'localhost'     # what IP address(es) to listen on;
+"""
+        expected_postgresql_content = """\
+# PostgreSQL configuration file
+#listen_addresses = 'localhost'     # what IP address(es) to listen on;
+listen_addresses = '*'
+"""
+        self._write_postgresql_file(postgresql_content)
+        ha_config = new_slave_ha_config('10.0.0.1')
+        postgres_updater = self._new_postgres_updater(ha_config)
+
+        postgres_updater.update_postgresql_file()
+
+        self.assertEqual(expected_postgresql_content, self._read_postgresql_file())
+
+    def test_update_postgresql_file_from_slave_to_slave(self):
+        postgresql_content = """\
+# PostgreSQL configuration file
+#listen_addresses = 'localhost'     # what IP address(es) to listen on;
+listen_addresses = '*'
+"""
+        expected_postgresql_content = postgresql_content
+        self._write_postgresql_file(postgresql_content)
+        ha_config = new_slave_ha_config('10.0.0.1')
+        postgres_updater = self._new_postgres_updater(ha_config)
+
+        postgres_updater.update_postgresql_file()
+
+        self.assertEqual(expected_postgresql_content, self._read_postgresql_file())
+
+    def test_update_postgresql_file_from_slave_to_non_slave(self):
+        postgresql_content = """\
+# PostgreSQL configuration file
+#listen_addresses = 'localhost'     # what IP address(es) to listen on;
+listen_addresses = '*'
+"""
+        expected_postgresql_content = """\
+# PostgreSQL configuration file
+#listen_addresses = 'localhost'     # what IP address(es) to listen on;
+"""
+        self._write_postgresql_file(postgresql_content)
+        ha_config = new_disabled_ha_config()
+        postgres_updater = self._new_postgres_updater(ha_config)
+
+        postgres_updater.update_postgresql_file()
+
+        self.assertEqual(expected_postgresql_content, self._read_postgresql_file())
