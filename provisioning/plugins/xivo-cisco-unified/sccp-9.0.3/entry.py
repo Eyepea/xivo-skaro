94225ae03d8fba860d4f7a917ca40f243bbc80ab
Etienne Lessard <elessard@avencall.com>
2012-03-16 14:01:40 -0400

    Add xivo-cisco-sccp-9.0.3 plugin.
    Needed because libsccp does not support yet some feature present in
    firmware >= 9.1, and extensive tests have only been done with the
    9.0.3 firmware version.

---
+++ provisioning/plugins/xivo-cisco-unified/sccp-9.0.3/entry.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+
+sccpcommon = {}
+execfile_('sccpcommon.py', sccpcommon)
+
+
+MODELS = [u'7906G', u'7911G', u'7931G', u'7941G', u'7942G', u'7945G', u'7961G',
+          u'7962G', u'7965G', u'7970G', u'7971G', u'7975G']
+VERSION = u'9.0.3/SCCP'
+MODEL_VERSION = dict((m, VERSION) for m in MODELS)
+
+
+class CiscoSccpPlugin(sccpcommon['BaseCiscoSccpPlugin']):
+    IS_PLUGIN = True
+
+    pg_associator = sccpcommon['common']['BaseCiscoPgAssociator'](MODEL_VERSION)
