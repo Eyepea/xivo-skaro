55a325945c4460d3ac82292fef19e9327b208b52
Etienne Lessard <elessard@avencall.com>
2012-06-20 15:35:30 -0400

    fix import.

--- agid/xivo_agid/directory/data_sources/tests/test_ldap.py
+++ agid/xivo_agid/directory/data_sources/tests/test_ldap.py
@@ -22,7 +22,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import unittest
-from tests.mock import Mock
+from mock import Mock
 from xivo_cti.directory.data_sources.ldap import LDAPDirectoryDataSource
 
 class TestLDAPDirectoryDataSource(unittest.TestCase):
